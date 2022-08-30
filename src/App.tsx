696eedf6af1f5b7a62cc776d9bac0067fd81ade8
AnnAntonova2310 <anyashaida@mail.ru>
2022-08-30 19:08:09 +0300

    Unit 18

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className={'app-wrapper'}>
             <Header />
             <Navbar />
-            <Profile/>
+            {/*<Profile/>*/}
+            <div className={'app-wrapper-content'}>
+            <Dialogs />
+            </div>
         </div>
     );
 }
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -6,7 +6,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 function Profile() {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img
                     src={'https://avatars.mds.yandex.net/i?id=27b34dd5c13c8258aa150be326f90bc0-5478040-images-thumbs&n=13'}/>
