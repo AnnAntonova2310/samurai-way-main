2e72118df92fd02a79206bb565a8b903ea02fc31
AnnAntonova2310 <anyashaida@mail.ru>
2022-09-11 15:58:56 +0300

    Unit 31

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {DetailedHTMLProps, TextareaHTMLAttributes} from 'react';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
@@ -19,15 +19,23 @@ const MyPosts = (props: MyPostsPropsType) => {
     let postsElement =
         props.posts.map(p => <Post message={p.message} likes={p.likes}/>)
 
+    let newPostElement = React.createRef<any>();
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+        newPostElement.current.value='';
+    }
+
     return (
+
         <div>
             <h3> My Posts </h3>
             <div className={s.postsBlock}>
                 <div>
-                    <textarea> </textarea>
+                    <textarea ref={newPostElement}> </textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
 
                 <div className={s.posts}>
